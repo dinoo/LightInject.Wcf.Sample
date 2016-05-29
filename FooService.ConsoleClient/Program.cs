d8aeafb125529523714995cba76daf11feba79d1
Remco <remco.hijmans@cygate.se>
2016-05-29 09:05:24 +0200

    Added a ConsoleClient for testing the service.

---
+++ FooService.ConsoleClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooService.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fooClient = new Foo.FooServiceClient();
+            try
+            {
+                fooClient.ServiceAction();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
+            var barClient = new Bar.BarServiceClient();
+            try
+            {
+                barClient.ServiceAction();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+        }
+    }
+}
