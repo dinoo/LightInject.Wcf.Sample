bab098f1c7550ddf89725a76f22b0d974898f2ca
Bernhard Richter <bernhard.richter@gmail.com>
2016-05-30 10:18:08 +0200

    Added startup class

---
+++ FooService.IISHost/Startup.cs
@@ -0,0 +1,19 @@
+﻿using FooService.IISHost;
+
+[assembly: System.Web.PreApplicationStartMethod(typeof(Startup), "Initialize")]
+namespace FooService.IISHost
+{
+    using Library;
+    using LightInject;
+    using LightInject.Wcf;
+
+    public class Startup
+    {
+        public static void Initialize()
+        {
+            var container = new ServiceContainer();
+            container.RegisterFrom<CompositionRoot>();
+            LightInjectServiceHostFactory.Container = container;
+        }
+    }
+}
